c1c2139ed38bb9b249d20d9b071a704b0ac4162d
Bach Nguyen <bachnx2303@gmail.com>
2023-12-04 10:42:52 +0700

    optimize: Style for button see more coupon in deal page

--- src/Resoures/Views/deals/list-by-store.blade.php
+++ src/Resoures/Views/deals/list-by-store.blade.php
@@ -114,7 +114,7 @@
                         @if (isset($listCoupon) && !empty($listCoupon))
                             @include('frontend.common.widgets.list-coupon', ['coupons' => $listCoupon['result']['data'], 'store' => $store->title, 'date' => '(' .date('M d, Y'). ')', 'recordsCount' => $listCoupon['result']['recordsCount']])
                             <div class="view-more-alldeal">
-                                <a href="{{ route('frontend::store::listByStore', [ 'slug' => $store->slug ]) }}" class="view-more-alldeal-link">
+                                <a href="{{ route('frontend::store::listByStore', [ 'slug' => $store->slug ]) }}" class="btn btn-view-more view-more-alldeal-link">
                                     {{ sprintf('See all %s Coupons', $store->title) }}
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/> </svg>
                                 </a>
