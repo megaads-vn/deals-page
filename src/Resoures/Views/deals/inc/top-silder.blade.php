91cd154de722c88d049b1f148cfd3335ce8ccfdc
Bach Nguyen <bachnx2303@gmail.com>
2023-12-07 10:34:42 +0700

    fix: Link cate deal

--- src/Resoures/Views/deals/inc/top-silder.blade.php
+++ src/Resoures/Views/deals/inc/top-silder.blade.php
@@ -4,7 +4,7 @@
             <a target="_blank" href="{{ route('frontend::category::listByCategory',['slug' => $item->slug]) }}" class="top-brands-image round-100 bx-square">
                 <img class="category-image" src="{{ App\Utils\Utils::reSizeImage($item->image, 168, 0) }}" data-src="{{ App\Utils\Utils::reSizeImage($item->image, 168, 0) }}" alt="{{ $item->title }}">
             </a>
-            <a href="{{ route('frontend::category::listByCategory',['slug' => $item->slug]) }}" class="top-brands-link">
+            <a href="{{ route('frontend::category::deals',['slug' => $item->slug]) }}" class="top-brands-link">
                 {{ $item->title }}
             </a>
         </div>
