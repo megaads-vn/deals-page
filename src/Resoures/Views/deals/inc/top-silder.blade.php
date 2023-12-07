9b1989f8ce10e32ab0b926342ef710fff4cd17f0
Bach Nguyen <bachnx2303@gmail.com>
2023-12-07 10:40:58 +0700

    fix: Link category deals

--- src/Resoures/Views/deals/inc/top-silder.blade.php
+++ src/Resoures/Views/deals/inc/top-silder.blade.php
@@ -1,7 +1,7 @@
 <div class="alldeals-top-silder">
     @foreach ($categories as $item)
         <div class="top-brands-deals-item">
-            <a target="_blank" href="{{ route('frontend::category::listByCategory',['slug' => $item->slug]) }}" class="top-brands-image round-100 bx-square">
+            <a target="_blank" href="{{ route('frontend::category::deals',['slug' => $item->slug]) }}" class="top-brands-image round-100 bx-square">
                 <img class="category-image" src="{{ App\Utils\Utils::reSizeImage($item->image, 168, 0) }}" data-src="{{ App\Utils\Utils::reSizeImage($item->image, 168, 0) }}" alt="{{ $item->title }}">
             </a>
             <a href="{{ route('frontend::category::deals',['slug' => $item->slug]) }}" class="top-brands-link">
